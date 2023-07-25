77a8aaa5fa2274e89b2aa0d45428a620a00e1d5e
xorgal <93151521+xorgal@users.noreply.github.com>
2023-07-26 00:39:32 +0300

    Update version v1.0.1

--- internal/file.go
+++ internal/file.go
@@ -37,7 +37,7 @@ var AllocatorDBFile = "allocdb"
 var BinaryMetadata = IBinaryMetadata{
 	BinaryFile:  "xtund",
 	Description: "Command Line Interface (CLI) tool for managing the xtun daemon",
-	Version:     "1.0.0",
+	Version:     "1.0.1",
 }
 
 var DirPath = IDirPath{
