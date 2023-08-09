b3933fbba122cae4bb162fcbb5deccc4389dd4a0
xorgal <93151521+xorgal@users.noreply.github.com>
2023-08-09 10:55:08 +0300

    Remove comments

--- cli/cli.go
+++ cli/cli.go
@@ -1,15 +1,3 @@
-// TODO:
-// 1. Refactor `rootCmd` to display help menu if no subcommand provided
-// 2. Interact with daemon by means of subcommands only
-// 3. Add more subcommands: `start`, `stop`, `reload`, `status`, `stats`, `reset`
-// 4. Refactor `init` subcommand so it makes initial configuration, but do not start systemd services
-//
-// Workflow example:
-// `xtund init -xzs :3001 -k xtun@2023`
-// `xtund start`
-// `xtund status`
-// `xtund stats`
-
 package cli
 
 import (
